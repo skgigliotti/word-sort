9e1ec5ea4f3f4abdf3d3ba4e9d4ff173736fab07
Sophia Gigliotti <skgigliotti@gmail.com>
2020-12-26 20:09:17 -0800

    Update files

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
-import vuetify from '@/plugins/vuetify' // path to vuetify export
+import App from './App.vue'
+
+Vue.config.productionTip = false
 
 new Vue({
-  vuetify,
+  render: h => h(App),
 }).$mount('#app')
